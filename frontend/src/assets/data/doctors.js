ada6b40c33114757d6de21beb516742b81a8b02e
Aman <jha04527@gmail.com>
2024-04-04 20:03:49 +0530

    Connect mongodb and JWT tokens

---
+++ frontend/src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+    {
+        id: "01",
+        name: "Dr. Naresh Trihan",
+        specialization: "Surgeon",
+        avgRating: 4.6,
+        totalRating: 272,
+        photo: doctorImg01,
+        totalPatients: 1500,
+        hospital: "Medhanta Hospital,Gurgaon.",
+    },
+    {
+        id: "02",
+        name: "Dr. Arbindo Das",
+        specialization: "Neurologist",
+        avgRating: 4.8,
+        totalRating: 278,
+        photo: doctorImg02,
+        totalPatients: 1800,
+        hospital: "National Hospital, Sangam Vihar.",
+    },
+    {
+        id: "03",
+        name: "Dr. Ajinkya Sharma",
+        specialization: "Dermatologist",
+        avgRating: 4.9,
+        totalRating: 300,
+        photo: doctorImg03,
+        totalPatients: 2010,
+        hospital: "Sabhdarjang , Delhi.",
+    },
+];
